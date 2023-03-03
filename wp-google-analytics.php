f7015b8ed97872b671ce7d9878be61ac49271c8b
8ctopus <hello@octopuslabs.io>
2023-03-03 10:49:34 +0400

    Improve script section using heredoc

--- wp-google-analytics.php
+++ wp-google-analytics.php
@@ -494,16 +494,19 @@ class wpGoogleAnalytics {
             $custom_vars[] = "_gaq.push(['_trackPageview']);";
         }
 
-        $async_code = "<script type='text/javascript'>
-    var _gaq = _gaq || [];
-    %custom_vars%
+        $async_code = <<<SCRIPT
+            <script type='text/javascript'>
+                var _gaq = _gaq || [];
+                %custom_vars%
 
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-</script>";
+                (function() {
+                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+            </script>
+        SCRIPT;
+
         $custom_vars_string = implode( "\r\n", $custom_vars );
         $async_code         = str_replace( '%custom_vars%', $custom_vars_string, $async_code );
 
